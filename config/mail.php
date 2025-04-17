83d1e2703b10c72c739ff02eaeedcf6f46592031
ceb1jadjaluddin <j.h.adjaluddin@gmail.com>
2025-04-17 14:33:04 +0800

    d
    d

--- config/mail.php
+++ config/mail.php
@@ -36,7 +36,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'server301.orangehost.com'),
+            'host' => env('MAIL_HOST', 'server307.orangehost.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
